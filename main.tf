a393829bdfc5621ad19619896003cdf243eb7e72
Iliyan Blagoev <i.blagoev@outlook.com>
2023-07-01 12:20:41 +0300

    Add additional node and VMs

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@
 
 resource "proxmox_vm_qemu" "radarr" {
   name        = "radarr"
-  target_node = var.tnode
-  clone       = var.template_name
+  target_node = var.tnode00
+  clone       = var.template_name_00
   full_clone  = true
   agent       = 1
   qemu_os     = "l26"
@@ -37,7 +37,7 @@ resource "proxmox_vm_qemu" "radarr" {
   cores       = 1
   sockets     = 1
   cpu         = "kvm64"
-  memory      = 650
+  memory      = 1024
   balloon     = 0
   scsihw      = "virtio-scsi-single"
 
@@ -50,8 +50,8 @@ resource "proxmox_vm_qemu" "radarr" {
 
 resource "proxmox_vm_qemu" "sonarr" {
   name        = "sonarr"
-  target_node = var.tnode
-  clone       = var.template_name
+  target_node = var.tnode00
+  clone       = var.template_name_00
   full_clone  = true
   agent       = 1
   qemu_os     = "l26"
@@ -62,7 +62,7 @@ resource "proxmox_vm_qemu" "sonarr" {
   cores       = 1
   sockets     = 1
   cpu         = "kvm64"
-  memory      = 512
+  memory      = 1024
   balloon     = 0
   scsihw      = "virtio-scsi-single"
 
@@ -74,8 +74,8 @@ resource "proxmox_vm_qemu" "sonarr" {
 }
 resource "proxmox_vm_qemu" "bazarr" {
   name        = "bazarr"
-  target_node = var.tnode
-  clone       = var.template_name
+  target_node = var.tnode00
+  clone       = var.template_name_00
   full_clone  = true
   agent       = 1
   qemu_os     = "l26"
@@ -86,7 +86,7 @@ resource "proxmox_vm_qemu" "bazarr" {
   cores       = 1
   sockets     = 1
   cpu         = "kvm64"
-  memory      = 512
+  memory      = 1024
   balloon     = 0
   scsihw      = "virtio-scsi-single"
 
@@ -99,8 +99,8 @@ resource "proxmox_vm_qemu" "bazarr" {
 
 resource "proxmox_vm_qemu" "jellyseerr" {
   name        = "jellyseerr"
-  target_node = var.tnode
-  clone       = var.template_name
+  target_node = var.tnode00
+  clone       = var.template_name_00
   full_clone  = true
   agent       = 1
   qemu_os     = "l26"
@@ -111,7 +111,7 @@ resource "proxmox_vm_qemu" "jellyseerr" {
   cores       = 1
   sockets     = 1
   cpu         = "kvm64"
-  memory      = 512
+  memory      = 1024
   balloon     = 0
   scsihw      = "virtio-scsi-single"
 
@@ -121,3 +121,53 @@ resource "proxmox_vm_qemu" "jellyseerr" {
     tag    = "50"
   }
 }
+resource "proxmox_vm_qemu" "mongodb" {
+  name        = "mongo"
+  target_node = var.tnode01
+  clone       = var.template_name_00
+  full_clone  = true
+  agent       = 1
+  qemu_os     = "l26"
+  ciuser      = "mongo_adm"
+  sshkeys     = var.sshkey
+  os_type     = "cloud-init"
+  bios        = "ovmf"
+  cores       = 1
+  sockets     = 2
+  cpu         = "host"
+  memory      = 8192
+  balloon     = 0
+  scsihw      = "virtio-scsi-single"
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = "50"
+  }
+}
+
+resource "proxmox_vm_qemu" "graylog" {
+  name        = "graylog"
+  target_node = var.tnode01
+  clone       = var.template_name_00
+  full_clone  = true
+  agent       = 1
+  qemu_os     = "l26"
+  ciuser      = "glog_adm"
+  sshkeys     = var.sshkey
+  os_type     = "cloud-init"
+  bios        = "ovmf"
+  cores       = 2
+  sockets     = 2
+  cpu         = "kvm64"
+  memory      = 8192
+  balloon     = 0
+  scsihw      = "virtio-scsi-single"
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = "50"
+  }
+}
+
